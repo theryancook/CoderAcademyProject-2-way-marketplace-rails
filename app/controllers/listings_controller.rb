e14edad64b368a39dc15485c55d4240d172c3fd6
theryancook <ryancook12@gmail.com>
2020-05-12 12:20:30 +1000

    created forms

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,4 +4,23 @@ class ListingsController < ApplicationController
     def index
         @listings = Listing.all
     end
+
+    def new
+        @listing = Listing.new
+    end
+
+    def create
+        @listing = current_user.listings.create(listing_params)
+        if @listing.errors.any?
+            render "new"
+        else
+            redirect_to listings_path
+        end
+    end
+
+    private
+    def listing_params
+        params.require(:listing).permit(:title, :model, :description, :size, :price, :locatoin, :postcode, :availabiity)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
   get "/", to: "pages#home", as: "root" #want the route to actually be listings
+
   get "/listings", to: "listings#index", as: "listings"
+
+  get "/listings/new", to: "listings#new", as: "listing_path"
+
+  post "/listings", to: "listings#create"
+
 
 end
