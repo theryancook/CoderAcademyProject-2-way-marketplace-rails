069f77b090337756dcfedd8664cb51db438bb017
theryancook <ryancook12@gmail.com>
2020-05-15 15:33:50 +1000

    styling

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :size
-  # validates :title, :model, :description, :size_id, :price, :location, :postcode, presence: true
+  validates :title, :model, :description, :size_id, :price, :location, :postcode, presence: true
   has_one_attached :picture
 end
